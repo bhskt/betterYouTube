4088fca24ebefa774ae87b65b9691d3c4b15768d
Bhaskar Tiwari <bt@qbuzzer.com>
2015-11-13 22:02:28 +0530

    Version 1.3

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,46 +1,72 @@
+'use strict';
+
+var _temporalUndefined = {};
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
+function _temporalAssertDefined(val, name, undef) { if (val === undef) { throw new ReferenceError(name + ' is not defined - temporal dead zone'); } return true; }
+
 (function () {
     'use strict';
-    let findParent = function (node, query) {
-            let parent = node.parentElement;
-            while(parent && !parent.matches(query)) {
-                parent = parent.parentElement;
-            }
-            return parent;
-        },
-        port = chrome.runtime.connect();
-    port.onMessage.addListener(function (videos) {
+    var findParent = _temporalUndefined,
+        port = _temporalUndefined;
+
+    findParent = function findParent(node, query) {
+        var parent = _temporalUndefined;
+        parent = node.parentElement;
+        while (_temporalAssertDefined(parent, 'parent', _temporalUndefined) && parent && !(_temporalAssertDefined(parent, 'parent', _temporalUndefined) && parent).matches(query)) {
+            _temporalAssertDefined(_temporalAssertDefined(parent, 'parent', _temporalUndefined) && parent, 'parent', _temporalUndefined);
+
+            parent = (_temporalAssertDefined(_temporalAssertDefined(parent, 'parent', _temporalUndefined) && parent, 'parent', _temporalUndefined) && _temporalAssertDefined(parent, 'parent', _temporalUndefined) && parent).parentElement;
+        }
+        return _temporalAssertDefined(parent, 'parent', _temporalUndefined) && parent;
+    };
+
+    port = chrome.runtime.connect();
+    (_temporalAssertDefined(port, 'port', _temporalUndefined) && port).onMessage.addListener(function (videos) {
         console.log(videos);
         videos.forEach(function (video) {
-            let thumbnails = document.body.getElementsByTagName('img');
-            for(let thumbnailsCount = 0; thumbnailsCount < thumbnails.length; thumbnailsCount += 1) {
-                let thumbnail = thumbnails.item(thumbnailsCount),
-                    a = findParent(thumbnail, 'a[href*="watch?v=' + video.id + '"]');
-                if(a) {
-                    
-                    let byt = document.createElement('div');
-                    byt.setAttribute('class', 'better-youtube-0');
-                    byt.innerHTML = '<div class="byt-like-percent" style="width:' + video.statistics.likePercent + '%;"></div>';
-                    thumbnail.parentNode.insertBefore(byt, thumbnail);
+            var thumbnails = _temporalUndefined;
+            thumbnails = document.body.getElementsByTagName('img');
+            for (var thumbnailsCount = 0; thumbnailsCount < (_temporalAssertDefined(thumbnails, 'thumbnails', _temporalUndefined) && thumbnails).length; thumbnailsCount += 1) {
+                var thumbnail = _temporalUndefined,
+                    a = _temporalUndefined;
+                thumbnail = (_temporalAssertDefined(thumbnails, 'thumbnails', _temporalUndefined) && thumbnails).item(_temporalAssertDefined(thumbnailsCount, 'thumbnailsCount', _temporalUndefined) && thumbnailsCount);
+                a = (_temporalAssertDefined(findParent, 'findParent', _temporalUndefined) && findParent)(_temporalAssertDefined(thumbnail, 'thumbnail', _temporalUndefined) && thumbnail, 'a[href*="watch?v=' + video.id + '"]');
+                if (_temporalAssertDefined(a, 'a', _temporalUndefined) && a) {
+
+                    var byt = _temporalUndefined;
+                    byt = document.createElement('div');
+                    (_temporalAssertDefined(byt, 'byt', _temporalUndefined) && byt).setAttribute('class', 'better-youtube-0');
+                    (_temporalAssertDefined(byt, 'byt', _temporalUndefined) && byt).innerHTML = '<div class="byt-like-percent" style="width:' + video.statistics.likePercent + '%;"></div>';
+                    (_temporalAssertDefined(thumbnail, 'thumbnail', _temporalUndefined) && thumbnail).parentNode.insertBefore(_temporalAssertDefined(byt, 'byt', _temporalUndefined) && byt, _temporalAssertDefined(thumbnail, 'thumbnail', _temporalUndefined) && thumbnail);
                 }
             }
         });
     });
     new MutationObserver(function (mutations) {
-        let ids = new Set();
+        var ids = _temporalUndefined;
+        ids = new Set();
         mutations.forEach(function (mutation) {
-            for(let addedNodesCount = 0; addedNodesCount < mutation.addedNodes.length; addedNodesCount += 1) {
-                if(typeof mutation.addedNodes.item(addedNodesCount).querySelectorAll === 'function') {
-                    let thumbnails = mutation.addedNodes.item(addedNodesCount).getElementsByTagName('img');
-                    for(let thumbnailsCount = 0; thumbnailsCount < thumbnails.length; thumbnailsCount += 1) {
-                        let a = findParent(thumbnails.item(thumbnailsCount), 'a[href*="watch?v="]');
-                        if(a) {
-                            let id = a.href.match(/watch\?v=([A-Z0-9_\-]{11})/i);
-                            if(id && id[1]) {
-                                id = id[1];
-                                ids.add(id);
-                                if(ids.size === 20) {
-                                    port.postMessage([...ids]);
-                                    ids.clear();
+            for (var addedNodesCount = 0; addedNodesCount < mutation.addedNodes.length; addedNodesCount += 1) {
+                if (typeof mutation.addedNodes.item(_temporalAssertDefined(addedNodesCount, 'addedNodesCount', _temporalUndefined) && addedNodesCount).querySelectorAll === 'function') {
+                    var thumbnails = _temporalUndefined;
+                    thumbnails = mutation.addedNodes.item(_temporalAssertDefined(addedNodesCount, 'addedNodesCount', _temporalUndefined) && addedNodesCount).getElementsByTagName('img');
+                    for (var thumbnailsCount = 0; thumbnailsCount < (_temporalAssertDefined(thumbnails, 'thumbnails', _temporalUndefined) && thumbnails).length; thumbnailsCount += 1) {
+                        var a = _temporalUndefined;
+                        a = (_temporalAssertDefined(findParent, 'findParent', _temporalUndefined) && findParent)((_temporalAssertDefined(thumbnails, 'thumbnails', _temporalUndefined) && thumbnails).item(_temporalAssertDefined(thumbnailsCount, 'thumbnailsCount', _temporalUndefined) && thumbnailsCount), 'a[href*="watch?v="]');
+                        if (_temporalAssertDefined(a, 'a', _temporalUndefined) && a) {
+                            var id = _temporalUndefined;
+                            id = (_temporalAssertDefined(a, 'a', _temporalUndefined) && a).href.match(/watch\?v=([A-Z0-9_\-]{11})/i);
+                            if (_temporalAssertDefined(id, 'id', _temporalUndefined) && id && (_temporalAssertDefined(id, 'id', _temporalUndefined) && id)[1]) {
+                                _temporalAssertDefined(_temporalAssertDefined(id, 'id', _temporalUndefined) && id, 'id', _temporalUndefined);
+
+                                id = (_temporalAssertDefined(_temporalAssertDefined(id, 'id', _temporalUndefined) && id, 'id', _temporalUndefined) && _temporalAssertDefined(id, 'id', _temporalUndefined) && id)[1];
+
+                                (_temporalAssertDefined(ids, 'ids', _temporalUndefined) && ids).add(_temporalAssertDefined(id, 'id', _temporalUndefined) && id);
+                                if ((_temporalAssertDefined(ids, 'ids', _temporalUndefined) && ids).size === 20) {
+                                    (_temporalAssertDefined(port, 'port', _temporalUndefined) && port).postMessage([].concat(_toConsumableArray(_temporalAssertDefined(ids, 'ids', _temporalUndefined) && ids)));
+                                    (_temporalAssertDefined(ids, 'ids', _temporalUndefined) && ids).clear();
                                 }
                             }
                         }
@@ -48,8 +74,8 @@
                 }
             }
         });
-        if(ids.size) {
-            port.postMessage([...ids]);
+        if ((_temporalAssertDefined(ids, 'ids', _temporalUndefined) && ids).size) {
+            (_temporalAssertDefined(port, 'port', _temporalUndefined) && port).postMessage([].concat(_toConsumableArray(_temporalAssertDefined(ids, 'ids', _temporalUndefined) && ids)));
         }
-    }).observe(document, {childList: true, subtree: true});
+    }).observe(document, { childList: true, subtree: true });
 })();
--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,72 @@
+'use strict';
+
+var _temporalUndefined = {};
+
+function _temporalAssertDefined(val, name, undef) { if (val === undef) { throw new ReferenceError(name + ' is not defined - temporal dead zone'); } return true; }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+
 (function () {
     'use strict';
-    let key = 'AIzaSyAQ3pfh8dLvdLYX3SDN7feWUPNcygBi4Ck',
-        idsProcessed = new Set(),
-        idsCached = new Map();
+    var key = _temporalUndefined,
+        idsProcessed = _temporalUndefined,
+        idsCached = _temporalUndefined;
+    key = 'AIzaSyAQ3pfh8dLvdLYX3SDN7feWUPNcygBi4Ck';
+    idsProcessed = new Set();
+    idsCached = new Map();
     chrome.runtime.onConnect.addListener(function (port) {
         port.onMessage.addListener(function (idsReceived) {
-            let idsNew = idsReceived.filter((id) => !idsProcessed.has(id)),
-                idsOld = idsReceived.filter((id) => idsCached.has(id));
-            if(idsNew.length) {
-                let request = new XMLHttpRequest();
-                idsProcessed = new Set([...idsProcessed, ...idsNew]);
-                idsNew = idsNew.join(',');
-                request.onreadystatechange = function () {
-                    if(request.readyState === 4) {
-                        if(request.status === 200) {
-                            let response = JSON.parse(request.responseText);
-                            if(response.pageInfo.totalResults) {
-                                let videos = [];
-                                response.items.forEach(function (video) {
-                                    video.statistics.likePercent = parseInt(video.statistics.likeCount) * 100;
-                                    video.statistics.likePercent /= parseInt(video.statistics.likeCount) + parseInt(video.statistics.dislikeCount);
-                                    video.statistics.likePercent = Math.round(video.statistics.likePercent);
-                                    videos.push(video);
-                                    idsCached.set(video.id, video)
-                                });
-                                port.postMessage(videos);
+            var idsNew = _temporalUndefined,
+                idsOld = _temporalUndefined;
+            idsNew = idsReceived.filter(function (id) {
+                return !(_temporalAssertDefined(idsProcessed, 'idsProcessed', _temporalUndefined) && idsProcessed).has(id);
+            });
+            idsOld = idsReceived.filter(function (id) {
+                return (_temporalAssertDefined(idsCached, 'idsCached', _temporalUndefined) && idsCached).has(id);
+            });
+            if ((_temporalAssertDefined(idsNew, 'idsNew', _temporalUndefined) && idsNew).length) {
+                (function () {
+                    var request = _temporalUndefined;
+                    request = new XMLHttpRequest();
+
+                    _temporalAssertDefined(_temporalAssertDefined(idsProcessed, 'idsProcessed', _temporalUndefined) && idsProcessed, 'idsProcessed', _temporalUndefined);
+
+                    idsProcessed = new Set([].concat(_toConsumableArray(_temporalAssertDefined(_temporalAssertDefined(idsProcessed, 'idsProcessed', _temporalUndefined) && idsProcessed, 'idsProcessed', _temporalUndefined) && _temporalAssertDefined(idsProcessed, 'idsProcessed', _temporalUndefined) && idsProcessed), _toConsumableArray(_temporalAssertDefined(idsNew, 'idsNew', _temporalUndefined) && idsNew)));
+
+                    _temporalAssertDefined(_temporalAssertDefined(idsNew, 'idsNew', _temporalUndefined) && idsNew, 'idsNew', _temporalUndefined);
+
+                    idsNew = (_temporalAssertDefined(_temporalAssertDefined(idsNew, 'idsNew', _temporalUndefined) && idsNew, 'idsNew', _temporalUndefined) && _temporalAssertDefined(idsNew, 'idsNew', _temporalUndefined) && idsNew).join(',');
+
+                    (_temporalAssertDefined(_temporalAssertDefined(request, 'request', _temporalUndefined) && request, 'request', _temporalUndefined) && _temporalAssertDefined(request, 'request', _temporalUndefined) && request).onreadystatechange = function () {
+                        if ((_temporalAssertDefined(_temporalAssertDefined(request, 'request', _temporalUndefined) && request, 'request', _temporalUndefined) && _temporalAssertDefined(request, 'request', _temporalUndefined) && request).readyState === 4) {
+                            if ((_temporalAssertDefined(_temporalAssertDefined(request, 'request', _temporalUndefined) && request, 'request', _temporalUndefined) && _temporalAssertDefined(request, 'request', _temporalUndefined) && request).status === 200) {
+                                var response = _temporalUndefined;
+                                response = JSON.parse((_temporalAssertDefined(_temporalAssertDefined(request, 'request', _temporalUndefined) && request, 'request', _temporalUndefined) && _temporalAssertDefined(request, 'request', _temporalUndefined) && request).responseText);
+                                if ((_temporalAssertDefined(response, 'response', _temporalUndefined) && response).pageInfo.totalResults) {
+                                    (function () {
+                                        var videos = _temporalUndefined;
+                                        videos = [];
+                                        (_temporalAssertDefined(response, 'response', _temporalUndefined) && response).items.forEach(function (video) {
+                                            video.statistics.likePercent = parseInt(video.statistics.likeCount) * 100;
+                                            video.statistics.likePercent /= parseInt(video.statistics.likeCount) + parseInt(video.statistics.dislikeCount);
+                                            video.statistics.likePercent = Math.round(video.statistics.likePercent);
+                                            (_temporalAssertDefined(_temporalAssertDefined(videos, 'videos', _temporalUndefined) && videos, 'videos', _temporalUndefined) && _temporalAssertDefined(videos, 'videos', _temporalUndefined) && videos).push(video);
+                                            (_temporalAssertDefined(idsCached, 'idsCached', _temporalUndefined) && idsCached).set(video.id, video);
+                                        });
+                                        port.postMessage(_temporalAssertDefined(_temporalAssertDefined(videos, 'videos', _temporalUndefined) && videos, 'videos', _temporalUndefined) && _temporalAssertDefined(videos, 'videos', _temporalUndefined) && videos);
+                                    })();
+                                }
                             }
                         }
-                    }
-                };
-                request.open('GET', 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' + idsNew + '&key=' + key, true);
-                request.send();
+                    };
+                    (_temporalAssertDefined(_temporalAssertDefined(request, 'request', _temporalUndefined) && request, 'request', _temporalUndefined) && _temporalAssertDefined(request, 'request', _temporalUndefined) && request).open('GET', 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' + (_temporalAssertDefined(idsNew, 'idsNew', _temporalUndefined) && idsNew) + '&key=' + (_temporalAssertDefined(key, 'key', _temporalUndefined) && key), true);
+                    (_temporalAssertDefined(_temporalAssertDefined(request, 'request', _temporalUndefined) && request, 'request', _temporalUndefined) && _temporalAssertDefined(request, 'request', _temporalUndefined) && request).send();
+                })();
             }
-            if(idsOld.length) {
-                port.postMessage(idsOld.map((id) => idsCached.get(id)));
+            if ((_temporalAssertDefined(idsOld, 'idsOld', _temporalUndefined) && idsOld).length) {
+                port.postMessage((_temporalAssertDefined(idsOld, 'idsOld', _temporalUndefined) && idsOld).map(function (id) {
+                    return (_temporalAssertDefined(idsCached, 'idsCached', _temporalUndefined) && idsCached).get(id);
+                }));
             }
         });
     });
